1e7f2ee6609f23338d0359f7304229c3e6b1b276
Riley Anderson <scout1553@gmail.com>
2024-10-30 20:02:47 -0600

    Added outlines
    There's a bug that makes them spaz out, figuring out why its is doing that

--- The Cursed Deep/Assets/Scripts/OutlineSelection.cs
+++ The Cursed Deep/Assets/Scripts/OutlineSelection.cs
@@ -5,45 +5,49 @@ public class OutlineSelection : MonoBehaviour
     private Transform selectedObj;
     private Transform highlightedObj;
     private RaycastHit hit;
-    private LayerMask layerMask;
+    [SerializeField] private LayerMask layerMask;
 
-    private void Start()
-    {
-        layerMask = LayerMask.GetMask("PlayerInteractables");
-    }
-
-    public void Update()
+    /*public void Update()
     {
         if (highlightedObj != null)
         {
-            var outline = highlightedObj.GetComponent<Outline>();
-            if (outline != null)
-            {
-                outline.enabled = false;
-            }
+            DisableOutline(highlightedObj);
             highlightedObj = null;
         }
 
         if (Camera.main == null) return;
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        if (layerMask == 20)
         {
             highlightedObj = hit.transform;
             if (highlightedObj != selectedObj)
             {
-                var outline = highlightedObj.GetComponent<Outline>();
-                if (outline == null)
-                {
-                    outline = highlightedObj.gameObject.AddComponent<Outline>();
-                    outline.OutlineColor = Color.yellow;
-                    outline.OutlineWidth = 5;
-                }
-                outline.enabled = true;
+                EnableOutline(highlightedObj);
             }
             else
             {
                 highlightedObj = null;
             }
+        }
+    }*/
+
+    public void EnableOutline(Transform obj)
+    {
+        var outline = obj.GetComponent<Outline>();
+        if (outline == null)
+        {
+            outline = obj.gameObject.AddComponent<Outline>();
+            outline.OutlineColor = Color.yellow;
+            outline.OutlineWidth = 5;
+        }
+        outline.enabled = true;
+    }
+
+    public void DisableOutline(Transform obj)
+    {
+        var outline = obj.GetComponent<Outline>();
+        if (outline != null)
+        {
+            outline.enabled = false;
         }
     }
 }
