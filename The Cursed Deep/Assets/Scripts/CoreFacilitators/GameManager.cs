e90b81bf33d46b37f9d7f7ea77d7609405a2fe11
Riley Anderson <scout1553@gmail.com>
2024-11-18 22:41:48 -0700

    Doing some testing in Shop for build

--- The Cursed Deep/Assets/Scripts/CoreFacilitators/GameManager.cs
+++ The Cursed Deep/Assets/Scripts/CoreFacilitators/GameManager.cs
@@ -15,16 +15,23 @@ public class GameManager : MonoBehaviour
     
     private void Start()
     {
+        Debug.Log("GameManager Start");
         onStart.Invoke();
+        Debug.Log("GameManager Start Invoked");
         StartCoroutine(LateInit());
+        Debug.Log("GameManager LateInit Started");
     }
     
     private IEnumerator LateInit()
     {
         yield return _wffu;
+        Debug.Log("GameManager LateInit 1");
         yield return _wffu;
+        Debug.Log("GameManager LateInit 2");
         yield return _wffu;
+        Debug.Log("GameManager LateInit 3");
         onLateInit.Invoke();
+        Debug.Log("GameManager LateInit Invoked");
     }
 
     public void StartGame()
